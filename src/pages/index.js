b63a4a44e1736847e3992ad3c4234e064168d456
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-11 19:29:32 +0900

    tutorial start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!!</div>
+  return (
+    <div>
+      <h1>Gatsby</h1>
+      <div>Hello world!!</div>
+    </div>
+  )
 }
