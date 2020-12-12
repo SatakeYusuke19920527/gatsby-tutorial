55ac6a245a4aeeb3db41e1cac894cbe554666b8b
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-12 14:45:29 +0900

    part2 fin

---
+++ src/components/container.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './container.module.css';
+const Container: React.FC = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Container;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/header';
+import './about-css-modules.module.css';
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
