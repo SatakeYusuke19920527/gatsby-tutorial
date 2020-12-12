2eeea2de6db4d10690115288d3dc0ec07c4de2d3
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-12 14:50:17 +0900

    part 3 start

--- src/components/container.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './container.module.css';
-const Container: React.FC = ({ children }) => {
-  return <div className="container">{children}</div>;
-};
-
-export default Container;
--- src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface PropsType {
-  headerText: string;
-}
-const Header: React.FC<PropsType> = (props) => {
-  return <h1>{props.headerText}</h1>;
-};
-
-export default Header;
